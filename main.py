35ad3eb2872eb42639225726196c454a881d4609
UsefulAli.com <g.ilnar1567@gmail.com>
2023-08-08 20:31:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from js import tg, user_id, user_firstname, username
 
-display(f"{user_firstname}! Заполните объявление!", target="hello")
+display(f"{user_firstname}! Заполните объявление!!!", target="hello")
