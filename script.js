b60c1bea9986d8d6a980500c6c5af41eceb74ef9
UsefulAli.com <g.ilnar1567@gmail.com>
2023-08-11 19:10:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,36 @@ tg.expand() // // метод позволяет растянуть окно на
 var helloElement = document.getElementById("hello");
 helloElement.textContent = `${user_firstname}, заполните объявление!`;
 
+let typeworkInput = document.getElementById("type-work");
+let cityInput = document.getElementById("city");
+let categoryworkInput = document.getElementById("category-work");
+let fioInput = document.getElementById("fio");
+let messageInput = document.getElementById("message");
+let currencyInput = document.getElementById("currency");
+let phoneInput = document.getElementById("phone");
+let submitButton = document.getElementById("buttonSumbit");
+
+submitButton.addEventListener('click', function() {
+    let typeworkValue = typeworkInput.value;
+    let cityValue = cityInput.value;
+    let categoryworkValue = categoryworkInput.value;
+    let fioValue = fioInput.value;
+    let messageValue = messageInput.value;
+    let currencyValue = currencyInput.value;
+    let phoneValue = phoneInput.value;
+
+    let alertMessage = `тип работы: ${typeworkValue}\nгород: ${cityValue}\nатегорию работы: ${categoryworkValue}\nимя: ${fioValue}\nо себе: ${messageValue}\nОплата: ${currencyValue}\nномер телефона: ${phoneValue}`;
+
+    alert(alertMessage);
+});
+
 
 // Отмена ввода при нажатии на свободное место
 const inputField = document.getElementById('inputField');
 document.addEventListener('click', function(event) {
   if (event.target !== inputField) {
     inputField.blur();
+  }
+});
   }
 });
